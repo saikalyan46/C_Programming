e19380586982d2a4358587974a4fcbf4d285b30a
saikalyan46 <saikalyan46@gmail.com>
2021-10-13 01:27:38 +0530

    Create check_char_in_a_string.c

---
+++ check_char_in_a_string.c
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int i;
+   char ch[20]="sai ram";
+  for(i=0;ch[i];i++) {
+      //printf("\n");
+      //printf("i:%d\n",i);
+      if(ch[i]=='a') {
+          printf("a is present");
+      }
+/*      else {
+            printf("a is not present");
+      }*/
+  }
+  
+    
+    return 0;
+}
